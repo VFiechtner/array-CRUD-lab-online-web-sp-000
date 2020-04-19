83d9cd9eecea586e356a4d957fa7cd8077a76bff
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-04-19 20:23:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  favorite_cats << "Taz"
+  favorite_cats.push("Taz")
 end
 
 def add_element_to_start_of_array(array, element)
